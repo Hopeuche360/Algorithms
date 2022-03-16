2cdec44dbdced14664b8774382997a9ca36a83cb
Hopeuche360 <hopechijuk@gmail.com>
2022-03-16 19:25:50 +0100

    plusMinus and staircase

--- src/com/company/hackerrank/PlusMinus.java
+++ src/com/company/hackerrank/PlusMinus.java
@@ -1,33 +1,29 @@
 package com.company.hackerrank;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PlusMinus {
     public static void main(String[] args) {
-        System.out.println(plusMinus(new ArrayList<>(Arrays.asList(1, 1, 0, -1, -1))));
+        plusMinus(new ArrayList<>(Arrays.asList(1, 1, 0, -1, -1)));
     }
 
-    public static int plusMinus(List<Integer> arr) {
+    public static void plusMinus(List<Integer> arr) {
         // Write your code here
-        int positive_count = 0;
-        int negative_count = 0;
-        int zero_count = 0;
+        double positive_count = 0;
+        double negative_count = 0;
+        double zero_count = 0;
 
         for (int i = 0; i < arr.size(); i++) {
             if (arr.get(i) > 0) positive_count++;
             else if (arr.get(i) < 0) negative_count++;
             else zero_count++;
         }
-
-        //there's a number format bug that needs to be fixed here
-        double positive_fraction = positive_count / arr.size();
-        double negative_fraction = negative_count / arr.size();
-        double zero_fraction = zero_count / arr.size();
-        System.out.format("%.5f\n", positive_fraction);
-        System.out.format("%.5f\n", negative_fraction);
-        System.out.format("%.5f", zero_fraction);
-        return positive_count;
+        System.out.printf("%.6g%n", positive_count / arr.size());
+        System.out.printf("%.6g%n", negative_count / arr.size());
+        System.out.printf("%.6g%n", zero_count / arr.size());
     }
 }
---
+++ src/com/company/hackerrank/Staircase.java
@@ -0,0 +1,17 @@
+package com.company.hackerrank;
+
+public class Staircase {
+    public static void main(String[] args) {
+        staircase(4);
+    }
+
+    public static void staircase(int n) {
+        // Write your code here
+        int count = 1;
+        String str = "";
+        while ( count <= n) {
+            str+= " ".repeat(n - count) + "#".repeat(count++) + "\n";
+        }
+        System.out.println(str);
+    }
+}
